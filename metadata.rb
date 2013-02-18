b9da39047ddb85919f7fa9ae4f85218a36ec346b
Leif Gensert <leif@propertybase.com>
2013-02-18 16:55:59 +0100

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'leif@propertybase.com'
 license          'MIT'
 description      'installs Oracle client on Ubuntu'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 %w{ubuntu debian}.each do |os|
   supports os
